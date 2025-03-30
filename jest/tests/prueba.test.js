959c7a172db8eaaa750492439d6def8541f54b40
Gianfranco Micheloni <gmicheloni52@gmail.com>
2025-03-30 02:05:54 -0300

    commit

---
+++ jest/tests/prueba.test.js
@@ -0,0 +1,5 @@
+describe("Una prueba de test que pasa siempre", () =>{
+    it("este test debe pasar siempre", () =>{
+        expect(true).toBe(true)
+    })
+})
---
+++ tarea.js
@@ -0,0 +1,5 @@
+function foo (a, b = 2){
+    console.log(a-b)
+}
+
+foo(5)
